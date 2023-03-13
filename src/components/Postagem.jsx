2930af85d49a30a872559d34024b691a87dedade
tgo-mas <talmeidasf@gmail.com>
2023-03-13 16:59:13 -0300

    Adicionando componente de Postagem.jsx

---
+++ src/components/Postagem.jsx
@@ -0,0 +1,9 @@
+export function Postagem(titulo, conteudo){
+    return(
+        <section>
+            <img src="https://picsum.photos/200" alt="Pic aleatória" />
+            <h1>{titulo}</h1>
+            <p>{conteudo}</p>
+        </section>
+    )
+}
