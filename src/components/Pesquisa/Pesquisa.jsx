13ddfe72e36e8b6f035eafbaa6c2dd0d60637bbd
tgo-mas <talmeidasf@gmail.com>
2023-03-17 10:42:46 -0300

    Correção da atividade Pesquisa: Adicionando imagens

--- src/components/Pesquisa/Pesquisa.jsx
+++ src/components/Pesquisa/Pesquisa.jsx
@@ -1,30 +1,31 @@
 import { useState } from "react"
 import { frutas } from "../../data/frutas.js"
+import "./Pesquisa.css"
 
-export function Pesquisa(){
+export function Pesquisa() {
 
     let [resultados, setResultados] = useState([]);
-    
-    function getFrutas(evt){
-        let frutasResult = frutas.filter((fruta) => fruta.nome.toLowerCase().includes(evt.target.value.toLowerCase()));
 
-        if(evt.target.value !== ""){
-            setResultados(frutasResult.map((fruta) => {
-                return(
-                    <div className="fruta">
-                        <h3>{fruta.nome}</h3>
-                    </div>
-                )
-            }));
-        }else{
-            setResultados([]);
-        }
+    function getFrutas(evt) {
+        let frutasResult = frutas.filter(
+            (fruta) => fruta.nome.toLowerCase().includes(evt.target.value.toLowerCase()));
+
+        setResultados(frutasResult.map((fruta) => {
+            return (
+                <div className="fruta">
+                    <img src={fruta.imagem} alt="fruta" />
+                    <h3>{fruta.nome}</h3>
+                </div>
+            )
+        }));
     }
 
     return (
         <div>
-            <input className="pesquisa" type="text" placeholder="Pesquise sua fruta.." onChange={getFrutas}/>
-            {resultados}
+            <input className="pesquisa" type="text" placeholder="Pesquise sua fruta.." onChange={getFrutas} />
+            <div className="lista-frutas">
+                {resultados}
+            </div>
         </div>
     )
 }
