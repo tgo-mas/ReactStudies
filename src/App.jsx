43eeadb35a56ab6c8e7cb80e37db5d112e5ff6a4
tgo-mas <talmeidasf@gmail.com>
2023-03-20 17:16:52 -0300

    Melhorias no componente CalcIMC

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@
 // import { Pesquisa } from "./components/Pesquisa/Pesquisa.jsx"
 // import { Sorteio } from "./components/Sorteio/Sorteio"
 // import { QuestaoQuiz } from "./components/QuestaoQuiz/QuestaoQuiz"
-// import { CalcIMC } from "./components/CalcIMC/CalcIMC"
+import { CalcIMC } from "./components/CalcIMC/CalcIMC"
 // import { Carrossel } from "./components/Carrossel/Carrossel"
 // import { ConversorTemp } from "./components/ConversorTemp/ConversorTemp"
-import { Dicionario } from "./components/Dicionario/Dicionario"
+// import { Dicionario } from "./components/Dicionario/Dicionario"
 
 export function App() {
     return (
         <>
-            <Dicionario/>
+            <CalcIMC/>
         </>
     )
 }
--- src/components/CalcIMC/CalcIMC.jsx
+++ src/components/CalcIMC/CalcIMC.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InputMask from "react-input-mask"
 import "./CalcIMC.css";
 
 export function CalcIMC() {
     const [imc, setImc] = useState('');
     const [statusImc, setStatusImc] = useState('');
+    const [peso, setPeso] = useState(0);
+    const [alt, setAlt] = useState(0);
 
-    function calcImc(){
-        const alt = Number.parseFloat(document.getElementById("alt").value);
-        const peso = Number.parseFloat(document.getElementById("peso").value);
-        console.log(alt + peso)
+    useEffect(() => {
         let numberImc = peso / Math.pow(alt, 2);
         setImc(numberImc);
         if(numberImc < 18.5){
@@ -25,7 +24,7 @@ export function CalcIMC() {
         }else if(numberImc > 40){
             setStatusImc("Obesidade Grau III");
         }
-    }
+    }, [alt, peso])
 
     return (
         <div className="calc">
@@ -33,13 +32,16 @@ export function CalcIMC() {
             <form>
                 <div className="input-box">
                     <label htmlFor="alt">Altura</label>
-                    <InputMask mask="9.99" id="alt" />
+                    <InputMask mask="9.99" id="alt" onChange={
+                        (event) => setAlt(Number.parseFloat(event.target.value))
+                    }/>
                 </div>
                 <div className="input-box">
                     <label htmlFor="peso">Peso</label>
-                    <InputMask mask="999.99" id="peso" />
+                    <InputMask mask="999.99" id="peso" onChange={
+                        (event) => setPeso(Number.parseFloat(event.target.value))
+                    } />
                 </div>
-                <button type="button" onClick={calcImc}>Calcular</button>
             </form>
             <h3>Resultado: {imc}</h3>
             <h3>{statusImc}</h3>
